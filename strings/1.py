e9017687b3c16ead1850ac2cbcecf2b0a473430e
dgusfr <diegogustavo.fr@gmail.com>
2024-11-05 12:03:54 -0300

    feat(): compara string

--- strings/1.py
+++ strings/1.py
@@ -2,13 +2,10 @@ def comparar_strings(string1, string2):
     tamanho_string1 = len(string1)
     tamanho_string2 = len(string2)
     
-    # Verifica se as strings têm o mesmo comprimento
     mesmo_comprimento = tamanho_string1 == tamanho_string2
     
-    # Verifica se as strings têm o mesmo conteúdo
     mesmo_conteudo = string1 == string2
     
-    # Imprime as informações
     print(f'String 1: {string1}\nTamanho de "{string1}": {tamanho_string1} caracteres')
     print(f'String 2: {string2}\nTamanho de "{string2}": {tamanho_string2} caracteres')
     
@@ -25,5 +22,4 @@ def comparar_strings(string1, string2):
 string1 = "Brasil Hexa 2006"
 string2 = "Brasil! Hexa 2006!"
 
-# Chama a função para comparar as strings
 comparar_strings(string1, string2)
