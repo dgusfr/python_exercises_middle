d4a31fb018c25838f1c8830ebb8c9979c4407c8e
dgusfr <diegogustavo.fr@gmail.com>
2024-11-05 12:14:41 -0300

    feat(vetores): par e impar

--- lists/5.py
+++ lists/5.py
@@ -4,11 +4,9 @@ for i in range(20):
     numero = float(input(f"Digite o {i + 1}º número real: "))
     vetor.append(numero)
 
-# Separando os números pares e ímpares em vetores diferentes
 pares = [num for num in vetor if num % 2 == 0]
 impares = [num for num in vetor if num % 2 != 0]
 
-# Imprimindo os vetores
 print("\nNúmeros digitados:")
 print(vetor)
 
