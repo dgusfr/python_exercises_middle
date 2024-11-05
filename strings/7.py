bf83fc6de114f829fcda62b2ccc9915b37fdbe67
dgusfr <diegogustavo.fr@gmail.com>
2024-11-05 12:10:06 -0300

    feat(): valida cpf

--- strings/7.py
+++ strings/7.py
@@ -1,12 +1,9 @@
 def validar_cpf(cpf):
-    # Remove caracteres não numéricos do CPF
     cpf_numerico = ''.join(filter(str.isdigit, cpf))
 
-    # Verifica se o CPF tem 11 dígitos
     if len(cpf_numerico) != 11:
         return False
 
-    # Calcula o primeiro dígito verificador
     soma = 0
     for i in range(9):
         soma += int(cpf_numerico[i]) * (10 - i)
@@ -14,7 +11,6 @@ def validar_cpf(cpf):
     if digito1 > 9:
         digito1 = 0
 
-    # Calcula o segundo dígito verificador
     soma = 0
     for i in range(10):
         soma += int(cpf_numerico[i]) * (11 - i)
@@ -22,16 +18,13 @@ def validar_cpf(cpf):
     if digito2 > 9:
         digito2 = 0
 
-    # Verifica se os dígitos verificadores calculados coincidem com os fornecidos
     if int(cpf_numerico[9]) == digito1 and int(cpf_numerico[10]) == digito2:
         return True
     else:
         return False
 
-# Solicita ao usuário que digite o CPF
 cpf = input("Digite o CPF no formato xxx.xxx.xxx-xx: ")
 
-# Chama a função para validar o CPF e exibe o resultado
 if validar_cpf(cpf):
     print("O CPF é válido.")
 else:
