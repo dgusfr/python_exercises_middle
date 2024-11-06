b6f151687e0e5a11197d33f42fa56060d0049895
dgusfr <diegogustavo.fr@gmail.com>
2024-11-06 15:46:32 -0300

    feat(print)

--- functions/1.py
+++ functions/1.py
@@ -1,6 +1,5 @@
 def imprimir_padrao(n):
     for i in range(1, n + 1):
-        #cria lista com número i repetido i vezes, concatenado por 'join' com espaço ' '
         linha = ' '.join([str(i)] * i)
         print(linha)
 
