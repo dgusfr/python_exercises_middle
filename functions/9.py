73c996dc1ba0e933d3973bff3a753aa7314c12d0
dgusfr <diegogustavo.fr@gmail.com>
2024-11-07 09:30:11 -0300

    feat(struct)

--- functions/9.py
+++ functions/9.py
@@ -3,7 +3,6 @@ import re
 def eh_palindromo(frase):
     frase_limpa = re.sub(r'[^a-zA-Z0-9]', '', frase.lower())
     return frase_limpa == frase_limpa[::-1]
-    # A função retorna True se a string limpa for igual à sua inversa, indicando que é um palíndromo
 
 # Solicitar entrada do usuário
 frase_input = input("Informe uma frase para verificar se é um palíndromo: ")
